a0ba2e76ba791bd5ca09f88c1f6a4cc2d2ac379e
mashhura20204 <ibraimovamashhura0412@gmail.com>
2025-12-09 16:54:47 +0500

    Update and rename README.md to main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+# mashq9_0912
+# 1-misol
+roy = [x**2 for x in range(1, 11)]
+print("royxat:", roy)
+
+# 2-misol
+juft_sonlar = [x for x in range(1, 21) if x % 2 == 0]
+print("2. Juft sonlar:", juft_sonlar)
+
+# 3-misol
+toq_sonlar = [x for x in range(10, 31) if x % 2 == 1]
+print("3. Toq sonlar:", toq_sonlar)
+
+# 4-misol
+sozlar = ["apple", "banana", "pear"]
+uzunliklar = [len(soz) for soz in sozlar]
+print("4. So'z uzunliklari:", uzunliklar)
+
+# 5-misol
+harflar = list("So'z")
+print("5. Harflar:", harflar)
+
+# 6-misol
+kublar = [x**3 for x in range(1, 6)]
+print("6. Sonlar kublari:", kublar)
